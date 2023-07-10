d075b41b07ea46dd2959f77d0dda9b0d5ef874dd
Mikhail Stepanov <mikhail.stepanovv@yandex.ru>
2023-07-10 16:27:00 +0400

    dot fix

--- src/pages/about.js
+++ src/pages/about.js
@@ -15,7 +15,7 @@ const About = ({ location, title, children }) => {
                     I contributed to <a href="https://www.aligntech.com/">Align Technology inc.</a>, <a href="https://dasreda.ru//">Business Environment (SberBank)</a>, <a href="https://www.docrobot.ru/">E-COM</a> and <a href="https://www.vtb.ru/n">VTB</a>.
                 </p>
                 <p>
-                    I always push myself to be a better developer and stay up to date with the latest technology trends. I'm also interested in ML (scikit-learn, pytorch) and mobile development (Flutter). You can find all my skill in my <a href="https://mikhail-stepanov.com/resume.pdf">CV.</a>. <br />
+                    I always push myself to be a better developer and stay up to date with the latest technology trends. I'm also interested in ML (scikit-learn, pytorch) and mobile development (Flutter). You can find all my skill in my <a href="https://mikhail-stepanov.com/resume.pdf">CV</a>. <br />
                 </p>
                 <p>
                     In my free time I play music, travel and climb.
