edcd052a3aae7c284532a53109d5de1cf3cb9452
Mikhail Stepanov <the.mikhail.stepanov@gmail.com>
2025-03-28 14:49:49 +0300

    update about section to include link to PhD program

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,7 +9,7 @@ const About = ({ location, title, children }) => {
             <section className='container page'>
                 <h1>About me</h1>
                 <p>
-                    Hi there! I am a backend tech lead and software engineer with 8 years of professional experience. I design software architecture on business need, build distributed, robust and fault tolerant services, manage and hire teams. I'm also interested in ML and some portion of frontend and mobile development. I am also finishing my PhD at NRU HSE.<br />
+                    Hi there! I am a backend tech lead and software engineer with 8 years of professional experience. I design software architecture on business need, build distributed, robust and fault tolerant services, manage and hire teams. I'm also interested in ML and some portion of frontend and mobile development. I am also finishing my <a href="https://www.hse.ru/staff/mstepanov">PhD at NRU HSE</a>.<br />
                 </p>
                 <p>
                     I contributed to architecture, design, integration and code base in <a href="https://www.aligntech.com/">Align Technology inc.</a>, <a href="https://dasreda.ru//">Business Environment (SberBank)</a>, <a href="https://www.docrobot.ru/">E-COM</a> and <a href="https://www.vtb.ru/n">VTB</a>.
