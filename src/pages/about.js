db14006b33f0a4e355f338d05c5712815161f48e
Mikhail Stepanov <the.mikhail.stepanov@gmail.com>
2025-08-19 15:54:42 +0300

    update about section to reflect current role and hobbies

--- src/pages/about.js
+++ src/pages/about.js
@@ -15,13 +15,13 @@ const About = ({ location, title, children }) => {
                     I contributed to architecture, design, integration and code base in <a href="https://www.aligntech.com/">Align Technology inc.</a>, <a href="https://dasreda.ru//">Business Environment (SberBank)</a>, <a href="https://www.docrobot.ru/">E-COM</a> and <a href="https://www.vtb.ru/n">VTB</a>.
                 </p>
                 <p>
-                    I'm an active CTO and co-founder in startup Oneum which is in production and ready to use state – <a href="https://oneum.io/">Oneum.io</a>.
+                    I'm an active Tech Lead and co-founder in startup Oneum which is in production and ready to use state – <a href="https://oneum.io/">Oneum.io</a>.
                 </p>
                 <p>
                     I always push myself to be a better lead, architect and developer and stay up to date with the latest technology trends. I'm also interested in ML and some portion of frontend and mobile development. You can check my resume here – <a href="https://mikhail-stepanov.com/resume.pdf">CV</a>. <br />
                 </p>
                 <p>
-                    In my free time I'm dog walker, amateur musician, traveler and climber. Gotta be yacht captain soon.
+                    In my free time I'm dog walker, amateur musician, traveler and climber. Gotta be sail yacht skipper soon.
                     <br />
                 </p>
             </section>
